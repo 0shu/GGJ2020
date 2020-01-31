43c265a9a946d734997923880c82a37f382a9d0c
P17160798 <reececarey@msn.com>
2020-01-31 22:02:01 +0000

    reece stuff

---
+++ Assets/Scripts/ReeceScripts/playerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GGJ2020
+{
+    public class playerController : MonoBehaviour
+    {
+        //Walk speed variable
+        public float walkSpeed;
+        public float jumpForce;
+
+        //Rigid body
+        Rigidbody rb;
+        Vector3 moveDirection;
+
+        public Transform groundCheck;
+        public float groundDistance = 0.04f;
+        public LayerMask groundMask;
+        bool isGrounded;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+            float horizontalMovement = Input.GetAxisRaw("Horizontal");
+            float verticalMovement = Input.GetAxisRaw("Vertical");
+
+            moveDirection = (horizontalMovement * transform.right + verticalMovement * transform.forward).normalized;
+
+            if (Input.GetKeyDown("space"))
+            {
+                if (isGrounded && verticalMovement < 0)
+                {
+                    rb.AddForce(transform.up * jumpForce);
+                }
+            }
+        }
+
+        void FixedUpdate()
+        {
+            Move();
+        }
+
+        void Move()
+        {
+            Vector3 yVelFix = new Vector3(0, rb.velocity.y, 0);
+            rb.velocity = moveDirection * walkSpeed * Time.deltaTime;
+            rb.velocity += yVelFix;
+        }
+    }
+}
---
+++ Assets/Scripts/ReeceScripts/playerInteraction.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGJ2020
+{
+    public class playerInteraction : MonoBehaviour
+    {
+        enum Tool {Pickaxe, Hammer, Bucket};
+        Tool currentTool;
+
+        public GameObject pickaxe;
+        public GameObject bucket;
+        public GameObject hammer;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Tool currentTool;
+            currentTool = Tool.Pickaxe;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown("1"))
+            {
+                currentTool = Tool.Pickaxe;
+                Interaction();
+            }
+            if (Input.GetKeyDown("2"))
+            {
+                currentTool = Tool.Bucket;
+                Interaction();
+            }
+            if (Input.GetKeyDown("3"))
+            {
+                currentTool = Tool.Hammer;
+                Interaction();
+            }
+        }
+
+        void Interaction()
+        {
+            switch (currentTool)
+            {
+                case Tool.Pickaxe:
+                    Instantiate(pickaxe);
+                    Debug.Log("Pickaxe");
+                    break;
+                case Tool.Bucket:
+                    Instantiate(bucket);
+                    Debug.Log("Bucket");
+                    break;
+                case Tool.Hammer:
+                    Instantiate(hammer);
+                    Debug.Log("Hammer");
+                    break;
+                default:
+                    Debug.Log("NOTHING");
+                    break;
+            }
+        }
+    }
+}
