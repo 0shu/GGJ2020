67599744c9f5299d81c5b4a0e8f8897b9c9d80f5
Joshua Simons <joshua.simons.contact@gmail.com>
2020-02-02 23:59:08 +0000

    Fixed exit bug
    Main menu buttons would be unpressable if you returned via dying to sheep whilst not focussed on the window

--- Assets/Scripts/Returner.cs
+++ Assets/Scripts/Returner.cs
@@ -17,6 +17,7 @@ public class Returner : MonoBehaviour
 
     IEnumerator ReturnHome()
     {
+        Cursor.lockState = CursorLockMode.None;
         yield return new WaitForSeconds(m_WaitTime);            //Waits for the amount of time
         Debug.Log("Changing scene back to : " + m_ReturnName);  //Lets us know what it's doing
         SceneManager.LoadScene(m_ReturnName);                   //Loads the other scene
